c7b6ed625cf0d7603b35076144e54e2acd54d48b
RitsukiGoto <kawakawaritsuki.dev@gmail.com>
2015-06-14 16:00:24 +0900

    並び替え、削除、追加用Activity実装 及び、その機能を実装

--- app/src/main/java/com/kawakawaplanning/rssreader/Activity/EditActivity.java
+++ app/src/main/java/com/kawakawaplanning/rssreader/Activity/EditActivity.java
@@ -1,17 +1,21 @@
 package com.kawakawaplanning.rssreader.Activity;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.kawakawaplanning.rssreader.R;
+import com.kawakawaplanning.rssreader.TouchListView;
 
 import org.json.JSONObject;
 
@@ -19,27 +23,40 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class EditActivity extends ActionBarActivity implements View.OnClickListener{
 
     public SharedPreferences sharedPref;
-    public String TitleDatas[];
-    public ListView lv;
+    public String TitleData[];
+    public String URLData[];
+    public TouchListView lv;
     public Button commitBtn;
     public EditText et;
+    public static Context context;
+    private ArrayList<String> array;
+    private IconicAdapter adapter=null;
+    private ArrayList<String> urlArray;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
 
+
+
+        context = this;
         sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
 
-        TitleDatas = sharedPref.getString("TitleData", "").split(",");
+        TitleData = sharedPref.getString("TitleData", "").split(",");
+        URLData = sharedPref.getString("URLData", "").split(",");
+        lv = (TouchListView)findViewById(R.id.list);
+        lv.setDropListener(onDrop);
+        lv.setRemoveListener(onRemove);
 
-        lv = (ListView)findViewById(R.id.editLv);
         setList();
 
 
@@ -48,6 +65,70 @@ public class EditActivity extends ActionBarActivity implements View.OnClickListe
 
         et = (EditText)findViewById(R.id.commitEt);
     }
+
+    private TouchListView.DropListener onDrop = new TouchListView.DropListener() {
+        @Override
+        public void drop(int from, int to) {
+            String title = adapter.getItem(from);
+            String url = urlArray.get(from);
+
+            adapter.remove(title);
+            adapter.insert(title, to);
+
+            urlArray.remove(from);
+            urlArray.add(to,url);
+
+            StringBuffer sb = new StringBuffer();
+            StringBuffer sb2 = new StringBuffer();
+
+            for(int i = 0 ;i < adapter.getCount();i++){
+                if(i == 0){
+                    sb.append(adapter.getItem(i));
+                    sb2.append(urlArray.get(i));
+
+                }else{
+                    sb.append("," + adapter.getItem(i));
+                    sb2.append("," + urlArray.get(i));
+                }
+            }
+            System.out.println(sb.toString());
+            System.out.println(sb2.toString());
+
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putString( "URLData",  sb2.toString());
+            editor.putString("TitleData", sb.toString());
+            editor.commit();
+        }
+    };
+
+    private TouchListView.RemoveListener onRemove = new TouchListView.RemoveListener() {
+        @Override
+        public void remove(int which) {
+            adapter.remove(adapter.getItem(which));
+            urlArray.remove(which);
+
+            StringBuffer sb = new StringBuffer();
+            StringBuffer sb2 = new StringBuffer();
+
+            for(int i = 0 ;i < adapter.getCount();i++){
+                if(i == 0){
+                    sb.append(adapter.getItem(i));
+                    sb2.append(urlArray.get(i));
+
+                }else{
+                    sb.append("," + adapter.getItem(i));
+                    sb2.append("," + urlArray.get(i));
+                }
+            }
+            System.out.println(sb.toString());
+            System.out.println(sb2.toString());
+
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putString( "URLData",  sb2.toString());
+            editor.putString("TitleData", sb.toString());
+            editor.commit();
+        }
+    };
 
     @Override
     public void onClick(View v) {
@@ -98,9 +179,31 @@ public class EditActivity extends ActionBarActivity implements View.OnClickListe
     }
 
     public void setList(){
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_expandable_list_item_1, TitleDatas);
+        array=new ArrayList<String>(Arrays.asList(TitleData));
+        urlArray = new ArrayList<String>(Arrays.asList(URLData));
+        adapter=new IconicAdapter();
         lv.setAdapter(adapter);
     }
+    class IconicAdapter extends ArrayAdapter<String> {
+        IconicAdapter() {
+            super(EditActivity.context, R.layout.row2, array);
+        }
 
+        public View getView(int position, View convertView,
+                            ViewGroup parent) {
+            View row = convertView;
+
+            if (row == null) {
+                LayoutInflater inflater = getLayoutInflater();
+
+                row = inflater.inflate(R.layout.row2, parent, false);
+            }
+
+            TextView label = (TextView) row.findViewById(R.id.label);
+
+            label.setText(array.get(position));
+
+            return (row);
+        }
+    }
 }
